e4d643902cce4838cdd71a5d2991d14c13bf5a63
DerekWW <derek.wierson@gmail.com>
2016-10-17 14:04:23 -0700

    change something in metal or user routes

---
+++ validations/users.js
@@ -0,0 +1,23 @@
+'user strict';
+
+const Joi =require('joi');
+
+module.exports.post = {
+  body: {
+    username: Joi.string()
+      .label('User Name')
+      .required()
+      .trim()
+      .min(1),
+    email: Joi.string()
+      .label('Email')
+      .required()
+      .email()
+      .trim(),
+    password: Joi.string()
+     .label('Password')
+     .required()
+     .trim()
+     .min(8)
+  }
+};
