8004d8d41a87b6c1a076f3cf17ec6106d74c86b2
DerekWW <derek.wierson@gmail.com>
2016-10-17 14:49:22 -0700

    update seed and migration files to include embed link

--- migrations/20161016204642_favorites.js
+++ migrations/20161016204642_favorites.js
@@ -15,6 +15,9 @@ exports.up = function(knex) {
       .inTable('users')
       .onDelete('CASCADE')
       .index();
+    table.string('embed_link')
+      .notNullable()
+      .defaultTo('')
     table.timestamps(true, true);
   });
 };
