d339bf417ebb6e2488901da66a8fed8d7f480801
Mstanton98 <mstanton98@gmail.com>
2016-10-17 11:27:01 -0700

    add users and token routes

--- seeds/2_tracks.js
+++ seeds/2_tracks.js
@@ -10,7 +10,8 @@ exports.seed = function(knex) {
         image_url: 'http://pantera.com/wp-content/uploads/2014/03/cfh800.jpg',
         sc_id: 272105509,
         created_at: new Date('2016-06-29 14:26:16 UTC'),
-        updated_at: new Date('2016-06-29 14:26:16 UTC')              }]);
+        updated_at: new Date('2016-06-29 14:26:16 UTC')
+      }]);
     })
     .then(() => {
       return knex.raw(
