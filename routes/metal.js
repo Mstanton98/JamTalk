5431d9f91148bd05f138862dd5eab9be1e4f86a9
Mstanton98 <mstanton98@gmail.com>
2016-10-17 14:51:10 -0700

    add to dos for metal route and played with css

--- routes/metal.js
+++ routes/metal.js
@@ -4,9 +4,16 @@ const boom = require('boom');
 const express = require('express');
 const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
+const request = require('request');
 
 const router = express.Router();
 
+//make request to api
+//build random function to get random song
+//once song is selected, send to database and build out object
+//interpolate the embed link as a string to contain the random songs id
+//send the object to front end via fetch or getJSON query
+//build out logic to rerun process every 12 hours
 router.get('/metal', (_req, res, next) => {
 
 })
