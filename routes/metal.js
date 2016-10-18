a8a356fb4f055d01ce541548cd730d611f20e133
Mstanton98 <mstanton98@gmail.com>
2016-10-17 19:22:59 -0700

    added knex stuff in metal

--- routes/metal.js
+++ routes/metal.js
@@ -15,31 +15,35 @@ console.log(randomNum);
 
 const router = express.Router();
 
-//make request to api
-//build random function to get random song
-//interpolate the embed link as a string to contain the random songs id
-//once song is selected, build out object and send to tracks table
 //send the object to front end via fetch or getJSON query
 //build out logic to rerun process every 12 hours
 router.get('/metal', (_req, res, next) => {
   request('https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud%3Agenres%3Ametal&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=50&offset=0&linked_partitioning=1&app_version=1476719521',(error, response, body) => {
-    if (error) throw new Error(error);
+    if (error) {
+      return next(boom.create(400, 'Bad Request'))
+    };
 
     const obj = JSON.parse(body);
     const newTrack = obj.collection[randomNum].track;
-    console.log(newTrack);
-    const track = {
+    const insertTrack = {
       genre: newTrack.genre,
       title: newTrack.title,
-      sc_id: newTrack.id,
-      embed_link: `<iframe scrolling="no" frameborder="no" id="song" src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${newTrack.id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'>`
+      scId: newTrack.id,
+      embedLink: `<iframe scrolling="no" frameborder="no" id="song" src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${newTrack.id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>`
       };
-    console.log(track);
 
-  console.log(randomNum);
-  res.send(obj.collection[randomNum]);
+      knex('tracks')
+        .insert(decamelizeKeys(insertTrack), '*')
+        .then((rows) => {
+          const track = camelizeKeys(row[0]);
+
+          res.send(track);
+        })
+        .catch((err) => {
+          next(err);
+        })
   });
-  })
 });
+
 
 module.exports = router;
