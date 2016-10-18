79819357a18835d97b6294be66cd35a443bd2d92
DerekWW <derek.wierson@gmail.com>
2016-10-18 10:11:03 -0700

    work on metal.js

--- routes/metal.js
+++ routes/metal.js
@@ -8,7 +8,6 @@ const {
   decamelizeKeys
 } = require('humps');
 const request = require('request');
-const timer = require('../timer').timer;
 let randomNum = Math.floor(Math.random() * 50);
 
 setInterval(() => {
@@ -22,7 +21,7 @@ const router = express.Router();
 //send the object to front end via fetch or getJSON query
 //build out logic to rerun process every 12 hours
 router.get('/metal', (_req, res, next) => {
-  request('https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud%3Agenres%3Ametal&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=50&offset=0&linked_partitioning=1&app_version=1476719521',(error, response, body) => {
+  request('https://api-v2.soundcloud.com/charts?kind=trending&genre=soundcloud%3Agenres%3Ametal&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=50&offset=0&linked_partitioning=1&app_version=1476719521',(error, response, body) => {
     if (error) {
       return next(boom.create(400, 'Bad Request'))
     };
@@ -36,6 +35,8 @@ router.get('/metal', (_req, res, next) => {
       embedLink: `<iframe scrolling="no" frameborder="no" id="song" src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${newTrack.id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>`
       };
 
+      console.log(newTrack);
+
       knex('tracks')
         .insert(decamelizeKeys(insertTrack), '*')
         .then((rows) => {
@@ -46,6 +47,7 @@ router.get('/metal', (_req, res, next) => {
         .catch((err) => {
           next(err);
         })
+
   });
 });
 
