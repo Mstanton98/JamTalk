a6d830cabb2a082158cff794a3df4f2197af6ae7
DerekWW <derek.wierson@gmail.com>
2016-10-19 16:41:34 -0700

    change metal in public

--- public/js/metal.js
+++ public/js/metal.js
@@ -1,13 +1,12 @@
 'use strict'
 
+
 $(function() {
 
   const $song = $('#songDiv');
   const $subminButton = $('');
   const $sidebarUsername = $('#username');
-  //const socket = io('/metal-chat');
 
-  $(".button-collapse").sideNav();
   // const socket = io();
 
   $.getJSON('/metal')
@@ -15,18 +14,12 @@ $(function() {
       $song.attr('src', track.embedLink);
     })
     .fail(() => {
-      Materialize.toast('Unable to retrieve song.  Default song playing.', 3000);
+      Materialize.toast('Unable to retrieve song', 3000);
     });
 
-  //   var socket = io('/metal-chat');
-  // $('#c').submit(function() {
-  //   socket.emit('chat message', $('#m').val());
-  //   $('#m').val('');
-  //   return false;
-  // });
-  // socket.on('chat message', function(msg) {
-  //   $('#messages').append($('<li>').text(msg));
-  // });
+
+
+
 
 
   // $('#c').submit(function() {
@@ -38,5 +31,17 @@ $(function() {
   //   $('#messages').append($('<li>').text(msg));
   // });
 
+  $(".button-collapse").sideNav();
 
+
+
+
+
+
+
+
+
+
+=======
+>>>>>>> socket.io
 });
